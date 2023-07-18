37c01e5d33d9d9bbf7626fcce6d578ce2d1082f8
Guide Fari <guide.fari@flexperto.com>
2023-07-18 06:26:43 +0200

    feat: improve error handling - improves variable names - removes unused code

--- src/http.test.ts
+++ src/http.test.ts
@@ -8,7 +8,6 @@ type TodoItem = {
 }
 
 test('http get request returns valid JSON', async () => { 
-  // TODO: nice to have - sane retry logic build right in
-  const Todo1 = await Effect.runPromise(http.get<TodoItem>('https://jsonplaceholder.typicode.com/todos/1'))
+  const Todo1 = await Effect.runPromise(http.get<TodoItem>('https://jsonplaceholder.typicode.com/todos/1a'))
   console.log('Todo1:', Todo1)
  })
--- src/http.ts
+++ src/http.ts
@@ -3,12 +3,11 @@ import { identity, pipe } from "@effect/data/Function"
 import * as Schema from "@effect/schema/Schema"
 
 
-const HttpError_ = Schema.struct({ _tag: Schema.literal("HttpError") })
-export interface HttpError extends Schema.To<typeof HttpError_> {}
-export const HttpError: Schema.Schema<HttpError> = Schema.to(HttpError_)
+const HttpErrorSchema = Schema.struct({ _tag: Schema.literal("HttpError"), message: Schema.string })
+export interface HttpError extends Schema.To<typeof HttpErrorSchema> {}
 
 export const get = <T>(url: string) =>
   Effect.tryCatchPromise(
     () => fetch(url).then((res) => res.status === 200 ? res.json() as T : Promise.reject()),
-    () => identity<HttpError>({ _tag: "HttpError" })
+    () => identity<HttpError>({ _tag: "HttpError", message: `Failed to fetch${url}` })
   )
