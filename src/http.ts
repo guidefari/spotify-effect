91b964c2d4ec0b05d78fff8d2d9fa7a47544a2f3
Guide Fari <guide.fari@flexperto.com>
2023-08-22 07:36:14 +0200

    refactor: type updates - prompted by rome lsp 🤔 - I do prefer the downstream type inference here

--- src/http.ts
+++ src/http.ts
@@ -1,13 +1,22 @@
-import * as Effect from "@effect/io/Effect"
-import { identity, pipe } from "@effect/data/Function"
-import * as Schema from "@effect/schema/Schema"
+import * as Effect from "@effect/io/Effect";
+import { identity, pipe } from "@effect/data/Function";
+import * as Schema from "@effect/schema/Schema";
 
-
-const HttpErrorSchema = Schema.struct({ _tag: Schema.literal("HttpError"), message: Schema.string })
-export interface HttpError extends Schema.To<typeof HttpErrorSchema> {}
+const HttpErrorSchema = Schema.struct({
+  _tag: Schema.literal("HttpError"),
+  message: Schema.string,
+});
+export type HttpError = Schema.To<typeof HttpErrorSchema>;
 
 export const get = <T>(url: string) =>
   Effect.tryCatchPromise(
-    () => fetch(url).then((res) => res.status === 200 ? res.json() as T : Promise.reject()),
-    () => identity<HttpError>({ _tag: "HttpError", message: `Failed to fetch${url}` })
-  )
+    () =>
+      fetch(url).then((res) =>
+        res.status === 200 ? (res.json() as T) : Promise.reject()
+      ),
+    () =>
+      identity<HttpError>({
+        _tag: "HttpError",
+        message: `Failed to fetch${url}`,
+      })
+  );
